d7feb61858f5ecd6242944ea5748855678c58edb
Valeria <valeriastetsiuk3546@gmail.com>
2022-09-09 16:03:18 +0300

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -15,62 +15,62 @@ const minutesTime = document.querySelector('span[data-minutes]');
 const secondsTime = document.querySelector('span[data-seconds]');
 let selectedTime = null;
 
-const options = {
-  enableTime: true,
-  time_24hr: true,
-  defaultDate: new Date(),
-  minuteIncrement: 1,
-  onClose(selectedDates) {
-    selectedTime = selectedDates[0].getTime();
-    if (Date.now() > selectedTime) {
-      btnStart.setAttribute('disabled', 'true');
-      return Notiflix.Notify.failure('Please choose a date in the future');
-    } else {
-      btnStart.removeAttribute('disabled');
-      Notiflix.Notify.success('Success');
-    }
-  },
-};
-const flatpickr = flatpickr('input#datetime-picker', options);
-btnStart.addEventListener('click', onStartTime);
+// const options = {
+//   enableTime: true,
+//   time_24hr: true,
+//   defaultDate: new Date(),
+//   minuteIncrement: 1,
+//   onClose(selectedDates) {
+//     selectedTime = selectedDates[0].getTime();
+//     if (Date.now() > selectedTime) {
+//       btnStart.setAttribute('disabled', 'true');
+//       return Notiflix.Notify.failure('Please choose a date in the future');
+//     } else {
+//       btnStart.removeAttribute('disabled');
+//       Notiflix.Notify.success('Success');
+//     }
+//   },
+// };
+// const flatpickr = flatpickr('input#datetime-picker', options);
+// btnStart.addEventListener('click', onStartTime);
 
-function onStartTime(evt) {
-  evt.preventDefault();
-  const time = setInterval(() => {
-    const currentTime = Date.now();
-    const watch = selectedTime - currentTime;
-    const { days, hours, minutes, seconds } = convertMs(watch);
-    daysTime.textContent = `${days}`;
-    hoursTime.textContent = `${hours}`;
-    minutesTime.textContent = `${minutes}`;
-    secondsTime.textContent = `${seconds}`;
-    if (watch <= 1000) {
-      clearInterval(time);
-      btnStart.disabled = true;
-      daysTime.textContent = 0;
-      hoursTime.textContent = 0;
-      minutesTime.textContent = 0;
-      secondsTime.textContent = 0;
-      return;
-    }
-  }, 1000);
-}
+// function onStartTime(evt) {
+//   evt.preventDefault();
+//   const time = setInterval(() => {
+//     const currentTime = Date.now();
+//     const watch = selectedTime - currentTime;
+//     const { days, hours, minutes, seconds } = convertMs(watch);
+//     daysTime.textContent = `${days}`;
+//     hoursTime.textContent = `${hours}`;
+//     minutesTime.textContent = `${minutes}`;
+//     secondsTime.textContent = `${seconds}`;
+//     if (watch <= 1000) {
+//       clearInterval(time);
+//       btnStart.disabled = true;
+//       daysTime.textContent = 0;
+//       hoursTime.textContent = 0;
+//       minutesTime.textContent = 0;
+//       secondsTime.textContent = 0;
+//       return;
+//     }
+//   }, 1000);
+// }
 
-function convertMs(ms) {
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
+// function convertMs(ms) {
+//   const second = 1000;
+//   const minute = second * 60;
+//   const hour = minute * 60;
+//   const day = hour * 24;
 
-  const days = addLeadingZero(Math.floor(ms / day));
-  const hours = addLeadingZero(Math.floor((ms % day) / hour));
-  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
-  const seconds = addLeadingZero(
-    Math.floor((((ms % day) % hour) % minute) / second)
-  );
+//   const days = addLeadingZero(Math.floor(ms / day));
+//   const hours = addLeadingZero(Math.floor((ms % day) / hour));
+//   const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+//   const seconds = addLeadingZero(
+//     Math.floor((((ms % day) % hour) % minute) / second)
+//   );
 
-  return { days, hours, minutes, seconds };
-}
-function addLeadingZero(value) {
-  return String(value).padStart(2, '0');
-}
+//   return { days, hours, minutes, seconds };
+// }
+// function addLeadingZero(value) {
+//   return String(value).padStart(2, '0');
+// }
